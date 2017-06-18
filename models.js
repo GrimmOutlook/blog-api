de3d5d91651ec8cffbf409a1c7ec8f48aa63237c
GrimmOutlook <grimmoutlook87@gmail.com>
2017-06-18 15:53:31 -0400

    Updated model.js to include schema.  Added mongoose & modified model & server to include it.

--- models.js
+++ models.js
@@ -1,3 +1,76 @@
+const mongoose = require('mongoose');
+
+const blogPostSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  author: {
+    firstName: String, required: true,
+    lastName: String, required: true
+  },
+  content: {type: String, required: true},
+  created: {date: Date}
+});
+
+
+blogPostSchema.virtual('authorString').get(() => {
+  return `${this.author.firstName} ${this.author.lastName}`});
+
+
+blogPostSchema.methods.apiRepr = () => {
+  return {
+    id: this._id,
+    title: this.title,
+    author: this.authorString,
+    content: this.content,
+    created: this.created
+  };
+}
+
+const blogPost = mongoose.model('blogPost', blogPostSchema);
+
+module.exports = {blogPost};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const uuid = require('uuid');
 
 // This module provides volatile storage, using a `BlogPost`
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 
 const app = express();
 
 const blogPostRouter = require('./blogPostRouter');
 
+const {PORT, DATABASE_URL} = require('./config');
+const {blogPost} = require('./models');
 
 app.use(morgan('common'));
-
 app.use(express.static('public'));
+app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
@@ -18,40 +24,40 @@ app.use('/blogpost', blogPostRouter);
 
 let server;
 
-// this function starts our server and returns a Promise.
-// In our test code, we need a way of asynchronously starting
-// our server, since we'll be dealing with promises there.
-function runServer() {
-  const port = process.env.PORT || 8080;
-  return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err)
-    });
-  });
-}
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
 
-// like `runServer`, this function also needs to return a promise.
-// `server.close` does not return a promise on its own, so we manually
-// create one.
-function closeServer() {
   return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
+    mongoose.connect(databaseUrl, err => {
       if (err) {
-        reject(err);
-        // so we don't also call `resolve()`
-        return;
+        return reject(err);
       }
-      resolve();
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
     });
   });
 }
 
-// if server.js is called directly (aka, with `node server.js`), this block
-// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
+  });
+}
+
+
 if (require.main === module) {
   runServer().catch(err => console.error(err));
 };
